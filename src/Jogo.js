4a170f8e947557bbe0dc927c9589f5c418981c60
Victor Mafra <victorvmafra97@gmail.com>
2023-02-23 16:46:17 -0300

    version 4 complete

--- src/Jogo.js
+++ src/Jogo.js
@@ -10,8 +10,8 @@ export default function Jogo(props) {
             <img src={props.images[props.countingError]} className={`${props.classImage}`} data-test="game-image"></img>
             <button class="choose" onClick={props.startGame} disabled={props.disableFindWord} data-test="choose-word">Escolher Palavra</button>
         </div>
-        <div className="wordSelected">
-        {props.wordAppeared.map((au) => (<div className="letter-game"> <div class={`each-letter ${props.classGreen}`} data-test="word">{ au }</div> </div>))}
+        <div className="wordSelected" data-test="word">
+        {props.wordAppeared.map((au) => (<div className="letter-game"> <div className={`each-letter ${props.classGreen}`}>{ au }</div> </div>))}
         </div>
         </>
     )
